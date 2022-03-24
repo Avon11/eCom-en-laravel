c1f7bba3913ec508c3567616c2efe1e1a0e010f4
AbhinavRana <vishuchoudhary131@gmail.com>
2022-03-24 10:30:55 +0530

    Icon fixed

--- eComm-en/resources/views/master.blade.php
+++ eComm-en/resources/views/master.blade.php
@@ -8,7 +8,6 @@
     <!-- Latest compiled and minified CSS -->
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" href="D:\Study\laravel Ecom\eComm-en\resources\ionicons-2.0.1\css">
 
 <!-- Optional theme -->
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>

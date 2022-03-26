e2399d8d6aa8324f070a9830087c8519527cd2b7
AbhinavRana <vishuchoudhary131@gmail.com>
2022-03-26 11:17:56 +0530

    Final V.1

--- eComm-en/resources/views/master.blade.php
+++ eComm-en/resources/views/master.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>E-comm Project</title>
+
     <!-- Latest compiled and minified CSS -->
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -60,7 +61,7 @@
         border-bottom: 1px solid #ccc;
         margin-bottom: 20px;
         padding-bottom: 20px;
-        
+
     }
 </style>
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
